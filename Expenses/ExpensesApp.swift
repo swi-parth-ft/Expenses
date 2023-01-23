0089dbe658eb6e786403d87d3f6dde8e38b89534
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-01-23 15:07:14 -0500

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Parth Antala on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
